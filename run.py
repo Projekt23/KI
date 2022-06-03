de874c7d2c0902e55284b8aba4b3cd4f7d9397f4
nauben <40056731+nauben@users.noreply.github.com>
2022-06-03 08:55:39 +0200

    Remove app replace with run

---
+++ run.py
@@ -0,0 +1,29 @@
+"""Run server"""
+import os
+import unittest
+
+from werkzeug.serving import run_simple
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from src import create_app
+
+if os.path.exists('.env'):
+    print('Importing environment from .env file')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
+#if os.environ.get('KI_BACKEND') == 'prod':
+#    app = DispatcherMiddleware(create_app(os.environ.get("KI_BACKEND")), {
+#        '/': create_app('prod')
+#    })
+#else:
+#    app = create_app(os.environ.get("KI_BACKEND") or 'dev')
+app = create_app(os.environ.get("KI_BACKEND"))
+
+if __name__ == '__main__':
+    if os.environ.get('KI_BACKEND') == 'prod':
+        run_simple('0.0.0.0', 5000, app,
+                   use_reloader=False, use_debugger=False,
+                   )
