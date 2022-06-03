ec8c4c895ca18605a5e3e9859249a0231bbbb4c1
nauben <40056731+nauben@users.noreply.github.com>
2022-06-03 16:10:04 +0200

    Redo config and adding logstash support

--- config.py
+++ config.py
@@ -8,6 +8,17 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Base config."""
 
+    ELASTIC_ACTIVE = os.environ.get("ELASTIC_ACTIVE")
+    ELASTIC_SCHEME = os.environ.get("ELASTIC_SCHEME")
+    ELASTIC_HOST = os.environ.get("ELASTIC_HOST")
+    ELASTIC_PORT = os.environ.get("ELASTIC_PORT")
+
+    LOGSTASH_ACTIVE = os.environ.get("LOGSTASH_ACTIVE")
+    LOGSTASH_HOST = os.environ.get("LOGSTASH_HOST")
+    LOGSTASH_DB_PATH = os.environ.get("LOGSTASH_DB_PATH")
+    LOGSTASH_TRANSPORT = os.environ.get("LOGSTASH_TRANSPORT")
+    LOGSTASH_PORT = os.environ.get("LOGSTASH_PORT")
+
     @staticmethod
     def init_app(app):
         pass
--- run.py
+++ run.py
@@ -1,29 +1,24 @@
 """Run server"""
 import os
 import unittest
-
+import nltk
 from werkzeug.serving import run_simple
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
+from dotenv import load_dotenv
 from src import create_app
 
-if os.path.exists('.env'):
-    print('Importing environment from .env file')
-    for line in open('.env'):
-        var = line.strip().split('=')
-        if len(var) == 2:
-            os.environ[var[0]] = var[1]
+load_dotenv()  # take environment variables from .env.
 
-#if os.environ.get('KI_BACKEND') == 'prod':
+# if os.environ.get('KI_BACKEND') == 'prod':
 #    app = DispatcherMiddleware(create_app(os.environ.get("KI_BACKEND")), {
 #        '/': create_app('prod')
 #    })
-#else:
+# else:
 #    app = create_app(os.environ.get("KI_BACKEND") or 'dev')
 app = create_app(os.environ.get("KI_BACKEND"))
 
 if __name__ == '__main__':
-    import nltk
+
     nltk.download('wordnet')
 
     if os.environ.get('KI_BACKEND') == 'prod':
