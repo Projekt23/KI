83556aa944a2577687e47d8b5fac28eccef97aa4
nauben <40056731+nauben@users.noreply.github.com>
2022-06-03 09:02:37 +0200

    Added testing

---
+++ src/tests/skeleton_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from src import create_app
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_skeleton(self):
+        return True
+
+
+if __name__ == '__main__':
+    unittest.main()
